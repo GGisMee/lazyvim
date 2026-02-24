79bb908f88e916614413d412cdc32c31d61a2ebf
gustavg <gustav.gamstedt@outlook.com>
2026-02-24 19:49:13 +0100

    fix c++ lsp issue

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -7,12 +7,14 @@ return {
           offsetEncoding = { "utf-16" },
         },
         cmd = {
+          "env",
+          "LC_ALL=C",
           "clangd",
           "--background-index",
           "--clang-tidy",
           "--header-insertion=never",
           -- query-driver ska peka på ALLA möjliga kompilatorer du använder
-          "--query-driver=/nix/store/*-gcc-*/bin/g++,/nix/store/*-clang-*/bin/clang++",
+          "--query-driver=/usr/bin/g++,/usr/bin/clang++,/nix/store/*/bin/g++,/nix/store/*/bin/clang++",
         },
         init_options = {
           fallbackFlags = { "-std=c++20" },
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -2,6 +2,8 @@
 return {
   "mason-org/mason.nvim",
   opts = {
-    ensure_installed = {},
+    ensure_installed = {
+      "clangd",
+    },
   },
 }
